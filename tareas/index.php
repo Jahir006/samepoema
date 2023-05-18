1b3a2cca9a7d4f22538b9b53de61ed842e1c23fe
Jahir006 <125289172+Jahir006@users.noreply.github.com>
2023-05-18 11:38:30 -0700

    Add files via upload

---
+++ tareas/index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>PoemaTarea</title>
+</head>
+<body>
+    <center>
+    <h1> ASI ES </h1>
+    <?php
+    
+    echo "<pre> El cielo es de cielo,<br>
+    la nube es de tiza.<br>
+    La cara del sapo<br>
+    me da mucha risa.<br>
+    
+    La luna es de queso<br>
+    y el Sol es de sol.<br>
+    La cara del sapo<br>
+    me da mucha tos.</pre>"; 
+    ?>
+    </center>
+
+</body>    
